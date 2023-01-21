dc99a21c38ee1f946272e341656078d37b41c727
Matthew Drobnak <matthew@drobnak.com>
2023-01-21 00:03:31 +0000

    Move wellness CSV writing functions to util class.

--- convutils.py
+++ convutils.py
@@ -4,6 +4,8 @@ import csv
 import os
 import sys
 
+FIT_EPOCH_OFFSET = 631065600+31536000
+
 def get_arguments():
   parser = argparse.ArgumentParser(
                       prog = 'fbtocsv',
@@ -105,3 +107,64 @@ def write_sleep(full_date, garmin_start_ts, garmin_start_ts_not_utc, local_to_ut
   sleep_writer.writerow(sleep_event_sta)
   sleep_writer.writerow(sleep_event_stp)
   sleep_data_file.close()
+
+def write_wellness(out_dict, full_date, offset_ts_data, garmin_start_ts, garmin_start_ts_not_utc, hrsamples, day_rhr, seven_day_avg, file_id_number):
+  #FIXME: Some items unconditionally use local time... Fix that.
+  output_data = []
+  local_to_utc_diff = garmin_start_ts - garmin_start_ts_not_utc
+
+  # Build data for CSV file.
+  header        = [ "Type","Local Number","Message","Field 1","Value 1","Units 1","Field 2","Value 2","Units 2","Field 3","Value 3","Units 3", "Field 4","Value 4","Units 4","Field 5","Value 5","Units 5","Field 6","Value 6","Units 6" ]
+  file_id_def   = [ "Definition","0","file_id","serial_number","1",None,"time_created",1,"","manufacturer","1","","product","1","","number","1","","type","1","" ]
+  dev_inf_def   = [ "Definition","1","device_info","timestamp","1",None,"serial_number","1",None,"manufacturer","1",None,"product","1",None,"software_version","1", None ]
+  m_inf_def     = [ "Definition","4","monitoring_info","timestamp","1",None,"cycles_to_distance","2",None,"cycles_to_calories","2",None,"step_goal","2",None,"resting_metabolic_rate","1",None,"activity_type","2",None,"unk_enum","1",None ]
+  m_inf_tz_def  = [ "Definition","4","monitoring_info","timestamp","1",None,"local_timestamp","1",None,"cycles_to_distance","2",None,"cycles_to_calories","2",None,"step_goal","2",None,"resting_metabolic_rate","1",None,"activity_type","2",None,"unk_enum","1",None ]
+  m_def         = [ "Definition","6","monitoring","timestamp","1",None,"distance","1",None,"cycles","1",None,"activity_type","1",None ]
+  hr_def        = [ "Definition","7","monitoring","timestamp","1",None,"heart_rate","1",None ]
+  floor_def     = [ "Definition","8","monitoring","timestamp","1",None,"ascent","1",None ]
+  rhr_def       = [ "Definition","9","resting_heart_rate","timestamp","1",None,"seven_day_rhr","1",None,"daily_rhr","1",None ]
+
+  file_id_data  = [ "Data","0","file_id","serial_number","3411930528",None,"time_created",garmin_start_ts_not_utc,None,"manufacturer","1",None,"garmin_product","3851",None,"number",file_id_number,None,"type","32",None ]
+  dev_info_data = [ "Data","1","device_info","timestamp",garmin_start_ts_not_utc,"s","serial_number","3411930528",None,"manufacturer","1",None,"garmin_product","3851",None,"software_version","11.16",None ]
+  m_inf_data    = [ "Data","4","monitoring_info","timestamp",garmin_start_ts_not_utc,"s","cycles_to_distance","1.6198|2.4296","m/cycle","cycles_to_calories","0.047|0.1482","kcal/cycle","step_goal","10000|10000",None,"resting_metabolic_rate","1987","kcal / day","activity_type","6|0",None,None,None,None ]
+  m_inf_tz_data = [ "Data","4","monitoring_info","timestamp",garmin_start_ts,"s","local_timestamp",garmin_start_ts_not_utc,"s","cycles_to_distance","1.6198|2.4296","m/cycle","cycles_to_calories","0.047|0.1482","kcal/cycle","step_goal","10000|10000",None,"resting_metabolic_rate","1987","kcal / day","activity_type","6|0",None,None,None,None ]
+
+  wellness_filename  = "new-wellness-%s.csv" % full_date
+  wellness_data_file = open(wellness_filename, 'w')
+  wellness_writer    = csv.writer(wellness_data_file)
+
+  wellness_writer.writerow(header)
+  wellness_writer.writerow(file_id_def)
+  wellness_writer.writerow(file_id_data)
+  wellness_writer.writerow(dev_inf_def)
+  wellness_writer.writerow(dev_info_data)
+  if offset_ts_data:
+    wellness_writer.writerow(m_inf_def)
+    wellness_writer.writerow(m_inf_data)
+  else:
+    wellness_writer.writerow(m_inf_tz_def)
+    wellness_writer.writerow(m_inf_tz_data)
+  wellness_writer.writerow(m_def)
+  wellness_writer.writerow(hr_def)
+  wellness_writer.writerow(floor_def)
+  wellness_writer.writerow(rhr_def)
+
+  # Iterate through data building rows.
+  for key in sorted(out_dict):
+    if "steps" in out_dict[key]:
+      output_data.append(["Data",6,"monitoring","timestamp",key-FIT_EPOCH_OFFSET, "s", "distance", "{0:.3f}".format(out_dict[key]['distance']), "m", "steps",int(out_dict[key]['steps']),"cycles","activity_type","6",None])
+    if "heartRate" in out_dict[key]:
+      output_data.append(["Data",7,"monitoring","timestamp",key-FIT_EPOCH_OFFSET, "s", "heart_rate",out_dict[key]['heartRate'],"bpm"])
+    if "floors" in out_dict[key]:
+      output_data.append(["Data",8,"monitoring","timestamp",key-FIT_EPOCH_OFFSET, "s", "ascent", "{0:.3f}".format(out_dict[key]['floors']),"m"])
+  wellness_writer.writerows(output_data)
+
+  # Last item should be Resting Heart Rate data.
+  rhr_ts = garmin_start_ts + 86000 - local_to_utc_diff if offset_ts_data else garmin_start_ts + 86000
+  if hrsamples == 6:
+    wellness_writer.writerow( [ "Data", "9","resting_heart_rate", "timestamp", rhr_ts,None,"seven_day_rhr", seven_day_avg, None, "daily_rhr", day_rhr, None ] )
+  else:
+    wellness_writer.writerow( [ "Data", "9","resting_heart_rate", "timestamp", rhr_ts,None,"daily_rhr", day_rhr, None ] )
+
+  # Close file.
+  wellness_data_file.close()
